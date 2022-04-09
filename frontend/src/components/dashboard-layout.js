40f48dfc050b6db549e62bf7f3d5011a574d1b25
eilon weinfeld <eilon.weinfeld@fiverr.com>
2022-04-09 15:53:52 +0300

    done

--- frontend/src/components/dashboard-layout.js
+++ frontend/src/components/dashboard-layout.js
@@ -3,8 +3,6 @@ import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { DashboardNavbar } from './dashboard-navbar';
 import { DashboardSidebar } from './dashboard-sidebar';
-import SingleDao from "./SingleDAO/single-dao";
-import CustomizedList from "./SingleDAO/single-dao-2";
 
 const DashboardLayoutRoot = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -22,7 +20,23 @@ export const DashboardLayout = (props) => {
 
   return (
     <>
-      <CustomizedList></CustomizedList>
+      <DashboardLayoutRoot>
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+            flexDirection: 'column',
+            width: '100%'
+          }}
+        >
+          {children}
+        </Box>
+      </DashboardLayoutRoot>
+      <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
+      <DashboardSidebar
+        onClose={() => setSidebarOpen(false)}
+        open={isSidebarOpen}
+      />
     </>
   );
 };
